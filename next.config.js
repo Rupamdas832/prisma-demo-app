ac03db7c8d117019d151aecf6222f88f3ad1a484
Rupam Das <rupamdas832@gmail.com>
2023-08-29 22:29:57 +0530

    cors header added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,47 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src 'self';
+  style-src 'self';
+  font-src 'self';
+`;
+
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://prisma-demo-app.vercel.app",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
