971e71a2fb1b7fbff0c652053ab4fce3909fb46f
Rupam Das <rupamdas832@gmail.com>
2023-08-30 21:10:05 +0530

    Removed navigation loader

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,10 +1,8 @@
 import Navbar from "../navbar/Navbar";
-import NavigationLoader from "../navigationLoader/NavigationLoader";
 
 export default function Layout({ children }) {
   return (
     <div className="flex w-full items-center justify-center">
-      <NavigationLoader />
       <Navbar />
       <div>{children}</div>
     </div>
